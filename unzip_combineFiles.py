8fe06a4a2adb305405104c4c59673befd5b863ad
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-08-17 15:08:26 -0700

    Update unzip_combineFiles.py

--- unzip_combineFiles.py
+++ unzip_combineFiles.py
@@ -7,6 +7,7 @@ Created on Tue Aug 11 15:48:11 2020
 Unzipping tex files and combining preamble and main tex file 
 """
 import os
+import json
 from subprocess import call, run
 
 # path to the directory having zip files
@@ -16,17 +17,17 @@ os.chdir(dir_zip)
 # unzipping the tar files
 for index, zipfile in enumerate(os.listdir()):
     
-    # create directory as paper#
-    paper_number = os.join.path(dir_zip, 'paper{}'.format(index))
-    call(['mkdir', paper_number])
-    os.chdir(paper_number)
-    
-    # changing the format
-    zipfile_new = '{}.tar.gz'.format(zipfile)
-    run(['mv', zipfile, zipfile_new])
-    
-    # unzipping the tar file
-    run(['tar -xvzf {}'].format(zipfile_new))
+#    # create directory as paper#
+#    paper_number = os.join.path(dir_zip, 'paper{}'.format(index))
+#    call(['mkdir', paper_number])
+#    os.chdir(paper_number)
+#    
+#    # changing the format
+#    zipfile_new = '{}.tar.gz'.format(zipfile)
+#    run(['mv', zipfile, zipfile_new])
+#    
+#    # unzipping the tar file
+#    run(['tar -xvzf {}'].format(zipfile_new))
     
 # combining the preamble, bibtex, and main tex file --> reanme them main.tex
     # total number of tex files
@@ -37,6 +38,12 @@ for index, zipfile in enumerate(os.listdir()):
         files = {}
         # read all the tex files to know which one has \input or \import statement
         for tf in tex_files:
+            
+            # rank dictionary to rank the multiple tex files in the order they are imported
+            rank_dict = {}
+            rank_dict[tf] = 0
+            
+            # reading the tex file
             with open(tf) as TF:
                 lines = TF.readlines()
             
@@ -45,45 +52,61 @@ for index, zipfile in enumerate(os.listdir()):
                 if '\\input' in line:
                     # get the imported file name
                     imported_file = line[line.find('{')+1 : line.find('}')]
-                    temp.append([line_index, imported_file])
-            
+                    temp.append(imported_file)
             files[tf] = temp
-                
-        # files.keys() --> main.tex
-        # replace all the \input statements with the respective files in main tex file
-        main_file = [i for i in files.keys() if len(files[i]) !=0]
-        for mf in main_file:
-            with open(mf) as main:
-                main_lines = main.readlines()
             
-            for n in range(len(files[f])):
-                main_line_index = files[f][n][0]
-                file_to_replace = files[f][n][1]
-                
-                # checking index of file_to_replace in the tex_files
-                t_index = tex_files.index('{}.tex'.format(file_to_replace))
-                with open(tex_files[t_index]) as ftr:
-                    ftr_lines = ftr.readlines()
-                
-                f_temp = []
-                for ff_index, ff_line in enumerate(ftr_lines):
-                    if ff_index != main_line_index:
-                        f_temp.append(ff_line)
-                    else:
-                        for iff in ftr_lines:
-                            f_temp.append(iff)
+        
+        # rank the files or arrange them in order
+        key_list = list(files.keys()) 
+        val_list = list(files.values()) 
+        rem_keys = key_list
+        
+        i = len(key_list)
+        for keys in key_list:
+            if i >1:
+                # check the keys that has no values --> no file imported in this file
+                if files[keys] ==[]:
+                    val_index = [val_list.index(vl) for vl in val_list if keys in vl][0]
+                    CallingFile = key_list[val_index]
                     
-                f = f_temp
-        
-        os.chdir(paper_number)
-        src, dst = main_file, "main.tex"
-        os.rename(src, dst)
-        
+                    # open CallingFile and replace the "\input{keys}" --> entire file (as list)
+                    os.chdir()
+                    with open(CallingFile) as CF:
+                        CFlines = CF.readlines
+                    with open(keys) as K:
+                        Klines = K.readlines
+                    
+                    for CFl in CFlines:
+                        if "\input" in CFl:
+                            if CFl[CFl.find('{')+1 : CFl.find('}')] == keys:
+                                CFlines.replace(CFl, Klines)
+                    
+                    # re-writing the opened CallingFile
+                    os.chdir()
+                    with open(CallingFile, 'w') as CF:
+                        json.dump(CFlines, CF, indent=4)
+                    
+                    # remove the imported file from the files dictionary
+                    # also remove it from the values of the file it is imported to
+                    del files[keys]
+                    
+                    old_value = files[CallingFile]
+                    new_value = [i for i in old_value if i != keys]
+                    files[CallingFile] = new_value
+                    i-=1
         
     else:
         # rename the only tex file as main.tex
-        src, dst = tex_files[0], 'main.tex'
+        src, dst = tex_files[0], 'main_new.tex'
         os.rename(src, dst)
     
         
-            
+           #########################################
+           
+           
+
+    
+        
+        
+    
+    
