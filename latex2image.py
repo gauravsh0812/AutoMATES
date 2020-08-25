d6c66593e89f2f80748a4e0fc432fb8afede6cb2
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-08-25 15:35:34 -0700

    Update latex2image.py

--- latex2image.py
+++ latex2image.py
@@ -9,12 +9,9 @@ latex src eqns to ".png" images
 NOTE: Please change the paths as per your system
 """
 import os, json
-from subprocess import call, run
+import subprocess
+from shutil import copyfile
 from pdf2image import convert_from_path
-
-
-# initializing an array to store corect eqns
-src_latex_correct_eqns = []
 
 # creating a template for tex file
 def template(eqn, k):
@@ -28,26 +25,76 @@ def template(eqn, k):
                '\\end{document}'
     return(temp)
 
-   
+# function to create tex documents for each eqn in the folder
+def CreateTexDoc(eqns, keyword_dict, tex_folder):
+    # creating tex files of the eqns
+    for index, eqn in enumerate(eqns):
+        # checking \DeclareMathOperator
+        DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
+        # creating tex file
+        path_to_tex = os.path.join(tex_folder, "eqn{}".format(index))
+        with open(path, 'w') as f_input:
+            tex_file = f_input.write(template(eqn, DeclareMathOperator_in_eqn))
 
-n_papers = 6
-for n_paper in range(1,n_papers):
+# create pdf files for the tex documents of the folder
+def CreatePdf(tex_folder, pdf_folder, correct_tex_folder):
+    for index, tex in enumerate(tex_folder):
+        # grabbing tex document in tex folder
+        path_to_tex = os.path.join(tex_folder, tex)
+        # calling the tex documnets and running and storing pdf files generated via "pdflatex".
+        os.chdir(pdf_folder)
+        value = subprocess.call(['pdflatex', path_to_tex])
+        # removing log and aux file
+        subprocess.call(['del', '{}.aux'.format(index)])
+        subprocess.call(['del', '{}.log'.format(index)])            
+        # appending correct tex documents
+        if value == 0:
+            # copy correct tex documents form the tex_folder to correct_tex_folder
+            src, dst = path_to_tex, os.path.join(correct_tex_folder,tex) 
+            copyfile(src, dst)
     
-    # creating tex, pdf, image dir
-    tex_paper = r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\src_mml_converter\eqn\tex\tex_files\paper{}'.format(n_paper)
-    call(['mkdir', tex_paper])
-    pdf_paper = r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\src_mml_converter\eqn\tex\pdf_files\paper{}'.format(n_paper)
-    call(['mkdir', pdf_paper])
-    image_paper = r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\src_mml_converter\eqn\tex\images_1\paper{}'.format(n_paper)
-    call(['mkdir', image_paper])
+# create images of the correct tex documents --> using pdf file 
+def CreateImages(pdf_folder, image_folder):
+    for index, pdf in enumerate(pdf_folder):
+        # extracting the image of the pdf
+        path_to_pdf = os.path.join(pdf_folder, pdf) 
+        output_file = '{}'.format(index)
+        os.chdir(image_folder)
+        img = convert_from_path(path_to_pdf, fmt = 'png', output_folder = image_folder, output_file=output_file)
     
-    # reading eqns of paper# from src_latex_paper#
-    with open (r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\results_file\paper{}\src_latex_paper{}.txt'.format(n_paper, n_paper), 'r') as file:
+# paths
+base_dir = "/home/gauravs/Automates/results_file"
+# Latex_equations directory
+latex_equations = os.path.join(base_dir, "latex_equations")
+# tex_files dumping directory
+tex_files = os.path.join(base_dir, "tex_files")
+# directory to dump pdf of the tex files
+latex_pdf = os.path.join(base_dir, "latex_pdf")
+# directory to dump images of the tex file equations
+latex_images = os.path.join(base_dir, "latex_images")
+# directory to dump correct latex equations
+latex_correct_equations = os.path.join(base_dir, "latex_correct_equations")
+
+# loop through the folders
+for folder in os.listdir(latex_equations):
+        
+    # creating tex, pdf, image folders for each paper
+    tex_folder = os.path.join(tex_files, folder)
+    pdf_folder = os.path.join(latex_pdf, folder)
+    image_folder = os.path.join(latex_images, folder)
+    correct_tex_folder = os.path.join(latex_correct_equations, folder)
+    for f in [tex_folder, pdf_folder, image_folder, correct_tex_folder]:
+        if not os.path.exists(f):
+            subprocess.call(['mkdir', f])
+                
+    # reading eqns of paper from folder in latex_equations 
+    path_to_folder = os.path.join(latex_equations, folder)
+    main_file = os.path.join(path_to_folder, "latex_equations.txt")
+    with open (main_file, 'r') as file:
         eqns = file.readlines()
-    
     # Dealing with "/DeclareMathOperator"
-    
-    with open(r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\results_file\paper{}\DeclareMathOperator_paper{}.txt'.format(n_paper, n_paper), 'r') as file:
+    DMO_file = os.path.join(path_to_folder, "DeclareMathOperator_paper.txt")
+    with open(DMO_file, 'r') as file:
         DMO = file.readline()
     
     # initializing /DeclareMathOperator dictionary
@@ -56,42 +103,10 @@ for n_paper in range(1,n_papers):
         ibegin, iend = i.find('{'), i.find('}')
         keyword_dict[i[ibegin+1 : iend]] = i
     
-    
-    # creating tex files of the eqns
-    for index, eqn in enumerate(eqns):
-        
-        # checking \DeclareMathOperator
-        DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
-        
-        # creating tex file
-        path = r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\src_mml_converter\eqn\tex\tex_files\paper{}\{}.tex'.format(n_paper, index)
-        with open(path, 'w') as f_input:
-            tex_file = f_input.write(template(eqn, DeclareMathOperator_in_eqn))
-    
-#    # calling the tex files we have just created
-#    os.chdir(tex_paper)
-#    for i, f in enumerate(os.listdir()):
-#        
-#        # grabbing respective tex documents
-#        file = os.path.join(tex_paper, f)
-        
-        # running and storing pdf files generated via "pdflatex".
-            os.chdir(pdf_paper)
-            value = run(['pdflatex', tex_file])
-        
-        # removing log and aux file
-            call(['del', '{}.aux'.format(index)])
-            call(['del', '{}.log'.format(index)])            
-        
-            if value == 0:
-                src_latex_correct_eqns.append(eqn) 
-            
-              # extracting the image of the pdf
-                img_path = os.path.join(pdf_paper, '{}.pdf'.format(index)) 
-                output_file = '{}.png'.format(index)
-                os.chdir(image_paper)
-                img = convert_from_path(img_path, fmt = 'png', output_folder = image_paper, output_file=output_file)
-
-# dumping src_latex_correct_eqns as json file
-with open(r'C:\Users\gaura\OneDrive\Desktop\AutoMATES\REPO\results_file\src_latex_correct_eqns.txt', 'w') as file:
-    json.dump(src_latex_correct_eqns, file, indent = 4)
+    # calling function to create tex doc for the particular folder --> giving all latex eqns, DMO, and tex_folder path as input
+    CreateTexDoc(eqns, keyword_dict, tex_folder)
+    # create pdf of all the tex document we have just created using pdflatex
+    CreatePdf(tex_folder, pdf_folder, correct_tex_folder)
+    # create images of correct tex documents
+    CreateImages(pdf_folder, image_folder)
+   
