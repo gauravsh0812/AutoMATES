a0a070a9b95400da243a45d9bb4c03052cf5ebbb
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-08-24 21:16:44 -0700

    Update parsing_Latex_eqns_code.py

--- parsing_Latex_eqns_code.py
+++ parsing_Latex_eqns_code.py
@@ -107,74 +107,74 @@ for tex_folder in os.listdir(dir_path):
                 macro_in_eqn.pop((list(macro_posi.keys())[list(macro_posi.values()).index(mp[0])]))
              
             # initializing an indicator flag --> to check if there will be change in the equation
-            #indicator = False
+            indicator = False
             
             for m in macro_in_eqn.keys():
             # replacing the macros with parameters
                 if m in macro_eq and '#' in macro_in_eqn[m]:
-                    #try:
-                    ff = macro_in_eqn[m]
-                    if ff.find('{') != 0:
-                        n_var = int(ff[1]) 
+                    try:
+                        ff = macro_in_eqn[m]
+                        if ff.find('{') != 0:
+                            n_var = int(ff[1]) 
 
-                #check the number of parameter given upfront and grab that parameter 
-                    n_var_upfront = (ff.find('{')//3) - 1
-                    var_upfront = [ff[1+i*3] for i in range(1, n_var_upfront) if n_var_upfront !=0]
+                    #check the number of parameter given upfront and grab that parameter 
+                        n_var_upfront = (ff.find('{')//3) - 1
+                        var_upfront = [ff[1+i*3] for i in range(1, n_var_upfront) if n_var_upfront !=0]
 
-                #check the number of parameter given in the eqn and grab those parameter 
-                    n_var_rem = n_var - n_var_upfront
-                    n_loop = 0
-                    var_eqn = []
-                    eq_copy = macro_eq
-                    b = eq_copy.find(m)
-                    b_end = b+len(m)
-                    eq_copy = eq_copy[b_end: ]
-                    while n_loop < n_var_rem:
-                        eq_part1 = eq_copy.find("{")
-                        eq_part11 = eq_copy.find("}")
-                        var_eqn.append(eq_copy[eq_part1 +1 : eq_part11])
-                        eq_copy = eq_copy[eq_part11+1 : ]
-                        n_loop+=1
+                    #check the number of parameter given in the eqn and grab those parameter 
+                        n_var_rem = n_var - n_var_upfront
+                        n_loop = 0
+                        var_eqn = []
+                        eq_copy = macro_eq
+                        b = eq_copy.find(m)
+                        b_end = b+len(m)
+                        eq_copy = eq_copy[b_end: ]
+                        while n_loop < n_var_rem:
+                            eq_part1 = eq_copy.find("{")
+                            eq_part11 = eq_copy.find("}")
+                            var_eqn.append(eq_copy[eq_part1 +1 : eq_part11])
+                            eq_copy = eq_copy[eq_part11+1 : ]
+                            n_loop+=1
 
-                    list_var = var_upfront + var_eqn 
+                        list_var = var_upfront + var_eqn 
 
-            #make a dictionaries having parameters and there values
-                    temp_macro_dict = {}
-                    for a_ind, a in enumerate(list_var):
-                        temp_macro_dict['#{}'.format(a_ind+1)] = a 
+                #make a dictionaries having parameters and there values
+                        temp_macro_dict = {}
+                        for a_ind, a in enumerate(list_var):
+                            temp_macro_dict['#{}'.format(a_ind+1)] = a 
 
-            # replace the macro with the expanded form
-                    ff = ff[ff.find('{'): ]    
-                    for tmd in temp_macro_dict.keys():
-                        if tmd in ff:
-                            ff = ff.replace(tmd, temp_macro_dict[tmd])
+                # replace the macro with the expanded form
+                        ff = ff[ff.find('{'): ]    
+                        for tmd in temp_macro_dict.keys():
+                            if tmd in ff:
+                                ff = ff.replace(tmd, temp_macro_dict[tmd])
 
-                    macro_eq = macro_eq.replace(m,ff)
-                    #indicator = True
+                        macro_eq = macro_eq.replace(m,ff)
+                        indicator = True
 
-                    #except:
-                     #       print("MACRO are in wrong format")
+                    except:
+                         print("MACRO are in wrong format")
             
             # replacing the macros with no parameters
                 if m in macro_eq and '#' not in macro_in_eqn[m]:
-                #try:
-                    macro_eq = macro_eq.replace(m, macro_in_eqn[m])
-                    #print(m)
-                    #indicator = True
+                    try:
+                        macro_eq = macro_eq.replace(m, macro_in_eqn[m])
+                        #print(m)
+                        indicator = True
 
-                    #except:
-                     #   print("MACRO is in the wrong format")
+                    except:
+                        print("MACRO is in the wrong format")
             
             # there are no actual macros to replace --> i.e. all were a part of greek letters       
-                #else:
-                 #   indicator = False
+                else:
+                    indicator = False
             
-            return(macro_eq)#, indicator)
+            return(macro_eq, indicator)
         
         # if length of macro_posi = 0    
-        #else:
-         #   indicator = False
-          #  return(macro_eq, indicator)
+        else:
+            indicator = False
+            return(macro_eq, indicator)
     
     # cleaning eqn - part 1 --> removing label, text, intertext
     def Clean_eqn_1(eqn_1):
@@ -229,7 +229,7 @@ for tex_folder in os.listdir(dir_path):
                     print([bmc_index, emc_index, len_mc])
                 
                     
-                # spot the index of "&"
+                # position of "&" in equation
                     list_of_and_symbol = [index_ for index_ ,char in enumerate(eqn_2) if char == "&"]     
                     print(list_of_and_symbol)
                 # index of the code in between bmc_index to emc_index 
@@ -238,10 +238,18 @@ for tex_folder in os.listdir(dir_path):
                     print(index_nochange)
                     
                 # anywhere except index_nochange --> replace the "&" with ''   
-                    for los in list_of_and_symbol:
-                        if eqn_2[los] == "&" and los not in index_nochange:
-                            eqn_2[los] = ''
-                
+                    eqn_2_array = eqn_2.split("&")
+                    temp = ""
+                    for l in ranage(len(list_of_and_symbol)):
+                        if list_of_and_symbol[l] in index_nochange:
+                            temp += eqn_2_array[l] +  eqn_2_array[l+1]
+                        else:
+                            temp += eqn_2_array[l] + "&"+ eqn_2_array[l+1]
+                    #for los in list_of_and_symbol:
+                     #   if eqn_2[los] == "&" and los not in index_nochange:
+                      #      eqn_2[los] = ''
+                    eqn_2 = temp
+                    
             if not indicator_bmc:
                 eqn_2=eqn_2.replace('&', '')
         
