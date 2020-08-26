f90cee900f1cdec2312eb08e76eb46ec4407da64
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-08-25 23:50:45 -0700

    Update latex2image_single loop.py

--- latex2image_single loop.py
+++ latex2image_single loop.py
@@ -16,16 +16,7 @@ def template(eqn, DMOineqn):
     DMOeqn= ''
     for d in DMOineqn:
         DMOeqn += "{} \n".format(d)
-    
-    '''
-    temp ='\\documentclass{standalone}\n' \
-               '\\usepackage{amsmath}\n' \
-               '\\usepackage{amssymb}\n' \
-               f'{k} \n' \
-               '\\begin{document}\n' \
-               f'$\\displaystyle {{{{ {eqn} }}}} $\n' \
-               '\\end{document}'
-    '''
+   
     # writing tex document for respective eqn 
     temp1 = '\\documentclass{standalone}\n' \
                '\\usepackage{amsmath}\n' \
@@ -36,7 +27,45 @@ def template(eqn, DMOineqn):
     
     temp = temp1 + DMOeqn + temp2
     return(temp)
-		
+
+# function to create tex documents for each eqn in the folder
+def CreateTexDoc(eqn, keyword_dict, tex_folder):
+ 	# checking \DeclareMathOperator
+	DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
+	# creating tex file
+	path_to_tex = os.path.join(tex_folder, eqn)
+	with open(path_to_tex, 'w') as f_input:
+		tex_file = f_input.write(template(eqn, DeclareMathOperator_in_eqn))
+	
+	return tex_file
+
+# create pdf files for the tex documents of the folder
+def CreatePdf(tex_file, pdf_folder, correct_tex_folder):
+	# calling the tex documnets and running and storing pdf files generated via "pdflatex".
+	os.chdir(pdf_folder)
+	value = subprocess.Popen(['pdflatex', tex_file])
+	vakue.communicate()
+	retcode = value.returncode
+	if not retcode == 0:
+    	os.unlink("{}.pdf".format(tex_file.split(".")[0]))
+	else:
+		# copy correct tex documents form the tex_folder to correct_tex_folder
+		src, dst = path_to_tex, os.path.join(correct_tex_folder,tex) 
+		copyfile(src, dst)
+	# removing log and aux file
+	os.unlink("{}.log".format(tex_file.split(".")[0]))
+	subprocess.call(['del', '{}.aux'.format(index)])
+	subprocess.call(['del', '{}.log'.format(index)])            
+	return("{}.pdf".format(tex_file.split(".")[0]))
+	
+# create images of the correct tex documents --> using pdf file 
+def CreateImages(pdf_folder, pdf_file, image_folder):
+	# extracting the image of the pdf
+	path_to_pdf = os.path.join(pdf_folder, pdf_file) 
+	output_file = '{}'.format(pdf_file.split(".")[0])
+	os.chdir(image_folder)
+	img = convert_from_path(path_to_pdf, fmt = 'png', output_folder = image_folder, output_file=output_file)
+    
 # paths
 base_dir = "/home/gauravs/Automates/results_file"
 # Latex_equations directory
@@ -61,12 +90,7 @@ for folder in os.listdir(latex_equations):
     for f in [tex_folder, pdf_folder, image_folder, correct_tex_folder]:
         if not os.path.exists(f):
             subprocess.call(['mkdir', f])
-                
-    # reading eqns of paper from folder in latex_equations 
-    path_to_folder = os.path.join(latex_equations, folder)
-    main_file = os.path.join(path_to_folder, "latex_equations.txt")
-    with open (main_file, 'r') as file:
-        eqns = file.readlines()
+   
     # Dealing with "/DeclareMathOperator"
     DMO_file = os.path.join(path_to_folder, "DeclareMathOperator_paper.txt")
     with open(DMO_file, 'r') as file:
@@ -77,3 +101,18 @@ for folder in os.listdir(latex_equations):
     for i in DMO:
         ibegin, iend = i.find('{'), i.find('}')
         keyword_dict[i[ibegin+1 : iend]] = i
+	
+     # reading eqns of paper from folder in latex_equations 
+    path_to_folder = os.path.join(latex_equations, folder)
+    main_file = os.path.join(path_to_folder, "latex_equations.txt")
+    with open (main_file, 'r') as file:
+        eqns = file.readlines()
+	
+	for eqn in eqns:
+		# create tex document for eqn
+		tex_file = CreateTexDoc(eqn, keyword_dict, tex_folder)
+		# create pdf file
+		pdf_file = CreatePdf(tex_file, pdf_folder, correct_tex_folder)
+		# create image 
+		image_file = CreateImages(pdf_folder, pdf_file, image_folder)
+		
