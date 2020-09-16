5ff7ea788f58d9f45ffec9a6b3e8db9cb0a8d5d9
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-09-16 08:48:28 -0700

    Update tex_to_pdf_png.py

--- tex_to_pdf_png.py
+++ tex_to_pdf_png.py
@@ -8,6 +8,11 @@ from shutil import copyfile
 path = "/home/gauravs/Automates/results_file"
 TexFolderPath = os.path.join(path, "tex_files")
 for folder in os.listdir(TexFolderPath):
+    
+    # make latex_correct_eqn folder
+    latex_correct_equations_folder = os.path.join(path, f"latex_correct_equations/{folder}")
+    if not os.path.exists(latex_correct_equations_folder):
+        subprocess.call(['mkdir',latex_correct_equations_folder])
 
     # make results PDF directory
     eqn_tex_dst_root = os.path.join(path, f"latex_pdf/{folder}")
@@ -28,10 +33,8 @@ for folder in os.listdir(TexFolderPath):
         os.remove(os.path.join(eqn_tex_dst_root, f'{i}.log'))
 
         # copying the tex file to the correct latex eqn directory
-        latex_correct_equations_folder = os.path.join(path, f"latex_correct_equations/{folder}")
-        if not os.path.exists(latex_correct_equations_folder):
-            subprocess.call(['mkdir',latex_correct_equations_folder])
-        copyfile(os.path.join(tex_folder,texfile), os.path.join(os.path.join(latex_correct_equations_folder, f"{texfile}")))
+        if output.returncode==0:
+            copyfile(os.path.join(tex_folder,texfile), os.path.join(os.path.join(latex_correct_equations_folder, f"{texfile}")))
 
         try:
             os.remove(os.path.join(eqn_tex_dst_root, f'{i}.aux'))
