f1f237dfbc1da6ceab05e7851e7d86d18c9961d8
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-09-22 16:27:25 -0700

    Update LaTeX_eqns2TeX_files.py

--- LaTeX_eqns2TeX_files.py
+++ LaTeX_eqns2TeX_files.py
@@ -2,16 +2,8 @@
 
 import os, subprocess
 
-def template(eqn, DMOineqn, Macroineqn):
-    # arranging \DeclareMathOpertaor
-    
-    DMOeqn= ''
-    for d in DMOineqn:
-        DMOeqn += "{} \n".format(d)
-    
-    Macroeqn= ''
-    for m in Macroineqn:
-        Macroeqn += "{} \n".format(m)
+# Template for the TeX files
+def template(eqn, Preamble):
     
     # writing tex document for respective eqn 
     temp1 = '\\documentclass{standalone}\n' \
@@ -21,18 +13,77 @@ def template(eqn, DMOineqn, Macroineqn):
             f'$\\displaystyle {{{{ {eqn} }}}} $\n' \
             '\\end{document}'
     
-    temp = temp1 + Macroeqn + DMOeqn + temp2
+    temp = temp1 + Preamble + temp2
     return(temp)
 
 # function to create tex documents for each eqn in the folder
-def CreateTexDoc(eqns, keyword_dict, keyword_Macro_dict, tex_folder):
-    # creating tex files of the eqns
-    for index, eqn in enumerate(eqns):
-        # checking \DeclareMathOperator and Macros
-        DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
-        Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in eqn]
-        # creating tex file
-        path_to_tex = os.path.join(tex_folder, "eqn{}.tex".format(index))
-        with open(path_to_tex, 'w') as f_input:
-            tex_file = f_input.write(template(eqn, DeclareMathOperator_in_eqn, Macros_in_eqn))
-            f_input.close()
+def CreateTexDoc(eqns, keyword_dict, keyword_Macro_dict, tex_folder, TeX_name):
+   
+    # checking \DeclareMathOperator and Macros
+    DeclareMathOperator_in_eqn = [kw for kw in keyword_dict.keys() if kw in eqn]
+    Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in eqn]
+    Preamble = ''
+    for arr in [DeclareMathOperator_in_eqn, Macros_in_eqn]:
+        for d in arr:
+            Preamble += "{} \n".format(d)
+            
+    # creating tex file
+    path_to_tex = os.path.join(tex_folder, "eqn{}.tex".format(TeX_name))
+    with open(path_to_tex, 'w') as f_input:
+        f_input.write(template(eqn, Preamble))
+        f_input.close()
+
+def main(latex_equations, tex_files):
+    
+    # loop through the folders
+    for folder in os.listdir(latex_equations):     
+        # creating tex, pdf, image folders for each paper
+        tex_folder = os.path.join(tex_files, folder)
+        if not os.path.exists(tex_folder):
+            subprocess.call(['mkdir', tex_folder])
+                    
+        # reading eqns of paper from folder in latex_equations 
+        path_to_folder = os.path.join(latex_equations, folder)
+        
+        # Dealing with "/DeclareMathOperator"
+        DMO_file = os.path.join(path_to_folder, "DeclareMathOperator_paper.txt")
+        with open(DMO_file, 'r') as file:
+            DMO = file.readline()
+        
+        # initializing /DeclareMathOperator dictionary
+        keyword_dict={}
+        for i in DMO:
+            ibegin, iend = i.find('{'), i.find('}')
+            keyword_dict[i[ibegin+1 : iend]] = i
+        
+        # Dealing with "Macros"
+        Macro_file = os.path.join(path_to_folder, "Macros_paper.txt")
+        with open(Macro_file, 'r') as file:
+            Macro = file.readline()
+        
+        # initializing /Macros dictionary
+        keyword_Macro_dict={}
+        for i in Macro:
+            ibegin, iend = i.find('{'), i.find('}')
+            keyword_Macro_dict[i[ibegin+1 : iend]] = i
+        
+        for MF in os.listdir(path_to_folder):
+            if MF != "DeclareMathOperator_paper.txt" or MF!= "Macros_paper.txt":
+                main_file = os.path.join(path_to_folder, MF)
+                with open (main_file, 'r') as file:
+                    eqn = file.readlines()
+                    file.close()
+                
+                TeX_name = MF.split(".")[0]
+                # calling function to create tex doc for the particular folder --> giving all latex eqns, DMOs, Macros and tex_folder path as arguments
+                CreateTexDoc(eqn, keyword_dict, keyword_Macro_dict, tex_folder, TeX_name)
+
+if __name__ == "__main__":
+    # paths
+    base_dir = "/home/gauravs/Automates/results_file"
+    # Latex_equations directory
+    latex_equations = os.path.join(base_dir, "latex_equations")
+    # tex_files dumping directory
+    tex_files = os.path.join(base_dir, "tex_files")
+    
+    main(latex_equations, tex_files, latex_pdf, latex_images, latex_correct_equations)
