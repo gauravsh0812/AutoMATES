894786147e641160de38f1b9080d179b6c118418
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-09-24 21:37:07 -0700

    Update pdf2png.py

--- pdf2png.py
+++ pdf2png.py
@@ -1,25 +1,36 @@
+# RENDERING .png FROM THE pdf FILES OF THE EQUATIONS
 
-# creating image files
-image_directory = "/home/gauravs/Automates/results_file/latex_images"
-pdf_directory = "/home/gauravs/Automates/results_file/latex_pdf"
-for pdf_folder in os.listdir(pdf_directory):
-    path_to_folder = os.path.join(pdf_directory, pdf_folder)
-    # mkdir for images of the pdf_folder
-    path_to_image_folder = os.path.join(image_directory, pdf_folder)
-    print(pdf_folder)
+import os, subprocess
+from pdf2image import convert_from_path
+
+def main(pdf_file_path, pdf_file, image_folder):
     try:
-        if not os.path.exists(path_to_image_folder):
-            subprocess.call(["mkdir", path_to_image_folder])
-
-        for index, pdf in enumerate(os.listdir(path_to_folder)):
-            print(['pdf_folder --> {}; pdf_file --> {}'.format(pdf_folder, pdf)])
-            # extracting the image of the pdf
-            if pdf.split(".")[1] == "pdf":
-                path_to_pdf = os.path.join(path_to_folder, pdf)
-                output_file = '{}.png'.format(pdf.split(".")[0])
-                image_folder = path_to_image_folder
-                #os.chdir(image_folder)
-                img = convert_from_path(path_to_pdf, fmt = 'png', output_folder = image_folder, output_file=output_file)
+        # extracting the image of the pdf
+        output_file = '{}.png'.format(pdf_file.split(".")[0])
+        convert_from_path(pdf_file_path, fmt = 'png', output_folder = image_folder, output_file=output_file)
     except:
-        print("wrong pdf file")
+        pass
 
+if __name == "__main__":
+    # Paths
+    root = "/home/gauravs/Automates/results_file"
+    pdf_path = os.path.join(root, "latex_pdf")
+    image_path = os.path.join(root, "latex_images")
+    
+    for fldr in os.listdir(pdf_path):
+        # Path to folder containing all pdf files of specific paper
+        pdf_folder = os.path.join(pdf_path, fldr)
+        # mkdir image folder if not exists
+        image_folder = os.path.join(image_path, fldr)
+        if not exists(image_folder):
+            subprocess.call(["mkdir", image_folder])
+        
+        # Looping through pdf files
+        for pdf_file in os.listdir(fldr):
+            if pdf_file.split(".")[1] == "pdf":
+                pdf_file_path = os.path.join(pdf_folder, pdf_file) 
+                main(pdf_file_path, pdf_file, image_folder)
+            
+            
+    
+    
