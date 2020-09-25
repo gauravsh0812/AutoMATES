b3f027f48bf4aedbd8df05bad5c854a35a1cf5f8
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-09-24 23:40:55 -0700

    Update LaTeX_MathML_Converter_MathJax.py

--- LaTeX_MathML_Converter_MathJax.py
+++ LaTeX_MathML_Converter_MathJax.py
@@ -1,17 +1,15 @@
+# CONVERT LaTeX EQUATION TO MathML CODE USING MathJax
+
 import requests
 import subprocess, os
 import json
 import argparse
 
-
 def main(eqn_file, mml_path):
     # Define the webservice address
     webservice = "http://localhost:8081"
-    #print("eqn --> %s" %eqn)
     # Load the LaTeX string data
-    #with open(eqn, "rb") as e:
     latex_strs = json.load(open(eqn_file, "r"))
-    #print("latex_strs --> %s"%latex_strs)
 
     # Translate and save each LaTeX string using the NodeJS service for MathJax
     mml_strs = list()
@@ -33,32 +31,61 @@ def main(eqn_file, mml_path):
     json.dump(mml_strs, open(mml_path, "w"))
 
 if __name__ == "__main__":
-
+    # Paths
     root = "/home/gauravs/Automates/results_file"
-    # path to directory containing correct latex eqns
+    # Path to directory containing correct latex eqns
     folder_correct_latex_eqns = os.path.join(root, "latex_correct_equations")
-    # path to directory contain mml eqns
+    # Path to directory contain MathML eqns
     mml_dir = os.path.join(root, "Mathjax_mml")
 
     for folder in os.listdir(folder_correct_latex_eqns):
-        # array to keep track of the latex eqns 
-        Latex_strs_json = []
-        
+        # Array to keep track of the latex eqns 
+        Latex_strs_json = []       
+        # Creating folder for MathML codes for specific file
         mml_folder = os.path.join(mml_dir, folder)
-        # making folder for each each paper
         if not os.path.exists(mml_folder):
             subprocess.call(['mkdir', mml_folder])
-
+           
+        # Appending all the eqns of the folder/paper to Latex_strs_json along with their respective\
+        # Macros and Declare Math Operator commands.
+        
+        # Creating Macros dictionary
+        Macro_path = os.path.join(root, f"latex_equations/{folder}/Macros_paper.txt")
+        with open(Macro_file, 'r') as file:
+            Macro = file.readline()
+            file.close()
+        keyword_Macro_dict={}
+        for i in Macro:
+            ibegin, iend = i.find('{'), i.find('}')
+            keyword_Macro_dict[i[ibegin+1 : iend]] = i
+        
+        # Creating DMO dictionary
+        DMO_path = os.path.join(root, f"latex_equations/{folder}/DeclareMathOperator_paper.txt")
+        with open(DMO_file, 'r') as file:
+            DMO = file.readline()
+            file.close()
+        keyword_dict={}
+        for i in DMO:
+            ibegin, iend = i.find('{'), i.find('}')
+            keyword_dict[i[ibegin+1 : iend]] = i
+        
         for eqn in os.listdir(os.path.join(folder_correct_latex_eqns, folder)):
             file_name = eqn.split(".")[0]
             file_path = os.path.join(root, "latex_equations/{}/{}.txt".format(folder, file_name))
-            print(file_path)
-            text_eqn = open(file_path, "r")
-            Latex_strs_json.append(text_eqn.readlines()[0])
-
-        # read the text version of this eqn in latex_eqns file
-        #text_eqn = open("file_path", "r")
-        print("Latex strs json arry --> {}".format(Latex_strs_json))
+            text_eqn = open(file_path, "r").readlines()[0]
+            Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in text_eqn]
+            DMOs_in_eqn = [kw for kw in keyword_dict.keys() if kw in text_eqn]
+            
+            # Writing Macros, DMOs, and text_eqn as one string
+            MiE, DiE = "", ""
+            for macro in Macros_in_eqn:
+                MiE = MiE + macro + " "
+            for dmo in DMOs_in_eqn:
+                DiE = DiE + dmo + " "    
+            final_string = MiE + DiE + text_eqn
+            Latex_strs_json.append(final_string)
+        
+        # Dumping Latex_strs_json
         json.dump(Latex_strs_json, open(os.path.join(root, f"JSON_dir/{folder}"),"w"))
 
         # calling main(arg1, arg2) --> arg1: path to json file containing latex eqns
