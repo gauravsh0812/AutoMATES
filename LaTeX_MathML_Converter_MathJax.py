c3e9896d0cc45b5a4b581e38508d56200033466c
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-10-02 16:07:51 -0700

    Update LaTeX_MathML_Converter_MathJax.py

--- LaTeX_MathML_Converter_MathJax.py
+++ LaTeX_MathML_Converter_MathJax.py
@@ -5,12 +5,13 @@ import subprocess, os
 import json
 import argparse
 
+logs = []
 def main(eqn_file, mml_path):
     # Define the webservice address
     webservice = "http://localhost:8081"
     # Load the LaTeX string data
     latex_strs = json.load(open(eqn_file, "r"))
-
+    
     # Translate and save each LaTeX string using the NodeJS service for MathJax
     mml_strs = list()
     for latex in latex_strs:
@@ -32,12 +33,12 @@ def main(eqn_file, mml_path):
 
 if __name__ == "__main__":
     # Paths
-    root = "/home/gauravs/Automates/results_file"
+    root = "/projects/temporary/automates/er/gaurav/results_file"
     # Path to directory containing correct latex eqns
     folder_correct_latex_eqns = os.path.join(root, "latex_correct_equations")
     # Path to directory contain MathML eqns
     mml_dir = os.path.join(root, "Mathjax_mml")
-
+    
     for folder in os.listdir(folder_correct_latex_eqns):
         # Array to keep track of the latex eqns 
         Latex_strs_json = []       
@@ -69,27 +70,49 @@ if __name__ == "__main__":
             ibegin, iend = i.find('{'), i.find('}')
             keyword_dict[i[ibegin+1 : iend]] = i
         
-        for eqn in os.listdir(os.path.join(folder_correct_latex_eqns, folder)):
-            file_name = eqn.split(".")[0]
-            file_path = os.path.join(root, "latex_equations/{}/{}.txt".format(folder, file_name))
-            text_eqn = open(file_path, "r").readlines()[0]
-            Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in text_eqn]
-            DMOs_in_eqn = [kw for kw in keyword_dict.keys() if kw in text_eqn]
+        temp = {}
+        to_replace = ["\\ensuremath", "\\xspace", "\\aligned", "\\endaligned", "\\span"]
+        for index, eqn in enumerate(os.listdir(os.path.join(folder_correct_latex_eqns, folder))):
+            if "eqn" in eqn:
+                temp[eqn] = index
+                file_name = eqn.split(".")[0]
+                file_path = os.path.join(root, "latex_equations/{}/{}.txt".format(folder, file_name))
+                try:
+                    text_eqn = open(file_path, "r").readlines()[0]
+                    Macros_in_eqn = [kw for kw in keyword_Macro_dict.keys() if kw in text_eqn]
+                    DMOs_in_eqn = [kw for kw in keyword_dict.keys() if kw in text_eqn]
             
-            # Writing Macros, DMOs, and text_eqn as one string
-            MiE, DiE = "", ""
-            for macro in Macros_in_eqn:
-                MiE = MiE + macro + " "
-            for dmo in DMOs_in_eqn:
-                DiE = DiE + dmo + " "    
-            final_string = MiE + DiE + text_eqn
-            Latex_strs_json.append(final_string)
+                    # Writing Macros, DMOs, and text_eqn as one string
+                    MiE, DiE = "", ""
+                    for macro in Macros_in_eqn:
+                        MiE = MiE + macro + " "
+                    for dmo in DMOs_in_eqn:
+                        DiE = DiE + dmo + " "    
+                    string = MiE + DiE + text_eqn
+                    for tr in to_replace:
+                        string = string.replace(tr, "")
+                    
+                    final_string = ""
+                    for sub in string.split(" "):
+                        if "cong" in sub:
+                            sub = sub.replace("\\cong, "{\\cong}")
+                        if "mathbb" in sub:
+                            if sub[sub.find("\\mathbb")+7] != "{":
+                                mathbb_parameter = sub[sub.find("\\newcommand")+12 : sub.find("}")].replace("\\", "")
+                                sub = sub[:sub.find("\\mathbb")+7] + "{" + mathbb_parameter + "}" + sub[sub.find("\\mathbb")+7+len(mathbb_parameter):]
+                        
+                        final_string += sub + " "     
+                                              
+                    Latex_strs_json.append(final_string)
         
-        # Dumping Latex_strs_json
-        if not os.path.exists(os.path.join(root, f"JSON_dir/{folder}_MathJax")):
-            subprocess.call(["mkdir", os.path.join(root, f"JSON_dir_MathJax/{folder}")])
-        json.dump(Latex_strs_json, open(os.path.join(root, f"JSON_dir_MathJax/{folder}/strings.txt"),"w"))
-
-        # calling main(arg1, arg2) --> arg1: path to json file containing latex eqns
-        # args2: path to the json file to store converted mml eqns.
-        main(os.path.join(root, f"JSON_dir_MathJax/{folder}/strings.txt"), os.path.join(mml_folder, "MathJax_mml_codes.txt"))
+                    # Dumping Latex_strs_json
+                    if not os.path.exists(os.path.join(root, f"JSON_dir/{folder}_MathJax")):
+                        subprocess.call(["mkdir", os.path.join(root, f"JSON_dir_MathJax/{folder}")])
+                    json.dump(Latex_strs_json, open(os.path.join(root, f"JSON_dir_MathJax/{folder}/strings.txt"),"w"))
+                    # calling main(arg1, arg2) --> arg1: path to json file containing latex eqns
+                    # args2: path to the json file to store converted mml eqns.
+                    main(os.path.join(root, f"JSON_dir_MathJax/{folder}/strings.txt"), os.path.join(mml_folder, "MathJax_mml_codes.txt"))
+                except:
+                    pass                          
+  
+     json.dump(logs, open("/projects/temporary/automates/er/gaurav/results_file/MathJax_logs.txt", "w"))  
