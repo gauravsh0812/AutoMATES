4f1ece0388f44bc8e989e3000796a02d4036b0cf
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2020-10-14 22:19:13 -0700

    Update LaTeX_MathML_Converter_MathJax.py

--- LaTeX_MathML_Converter_MathJax.py
+++ LaTeX_MathML_Converter_MathJax.py
@@ -6,45 +6,21 @@ import json
 import argparse
 
 # Defining global dictionary and array to capture all the keywords not supported by MathJax and other erros respectively
-keywords_log = {}
+keywords_log =[]
 Errors = []
 
-def main(folder, eqn_file, mml_path):
-    # Define the webservice address
-    webservice = "http://localhost:8081"
-    # Load the LaTeX string data
-    latex_strs = json.load(open(eqn_file, "r"))
+# Removing "\n", "\", and un-necessary qoutes 
+def CleaningMML(res):
+    # Removing "\ and /" at the begining and at the end
+    res = res[res.find("<"):]
+    res = res[::-1][res[::-1].find(">"):]
+    res = res[::-1]
+    # Removing "\\n"
+    res = res.replace(">\\n", ">")
+    return(res)
     
-    # Translate and save each LaTeX string using the NodeJS service for MathJax
-    mml_strs = list()
-    for latex in latex_strs:
-        res = requests.post(
-            f"{webservice}/tex2mml",
-            headers={"Content-type": "application/json"},
-            json={"tex_src": json.dumps(latex)},
-             )
-        
-        # Capturing the keywords not supported by MathJax
-        if "FAILED" in res.content.decode("utf-8"):
-            # Just to check errors
-            TeXParseError = res.content.decode("utf-8").split("::")[1]
-            # Logging incorrect/ unsupported keywords along with their equations
-            if "Undefined control sequence" in TeXParseError:
-                Unsupported_Keyword = TeXParseError.split("\\")[-1]
-                if Unsupported_Keyword not in keywords_log.keys():
-                    keywords_log[f"{folder} --> {Unsupported_Keyword}"] = latex
-            # Logging errors other than unsupported keywords
-            else:
-                if TeXParseError not in Errors:
-                    Errors.append(TeXParseError)
-
-        # Save the MML text response to our list
-        mml_strs.append(res.text)
-
-    # Dump the MathML strings to JSON
-    json.dump(mml_strs, open(mml_path, "w"))
-
 def Creating_Macro_DMO_dictionaries(folder):
+    #print("in Creating_Macro_DMO_dictionaries")
     Macro_file = os.path.join(root, f"latex_equations/{folder}/Macros_paper.txt")
     with open(Macro_file, 'r') as file:
         Macro = file.readlines()
@@ -66,8 +42,8 @@ def Creating_Macro_DMO_dictionaries(folder):
     
     return(keyword_Macro_dict, keyword_dict)
 
-def Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, to_remove):
-    Latex_strs_json = []       
+def Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, Large_MML, Small_MML):
+    #print("in Creating_final_equations")
     for index, eqn in enumerate(os.listdir(ce)):
         file_name = eqn.split(".")[0]
         EqnsType = "Large_eqns" if ce == Large_correct_eqns else "Small_eqns"
@@ -87,7 +63,7 @@ def Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, to_re
             string = MiE + DiE + text_eqn
             
             # Removing unsupported keywords 
-            for tr in to_remove:
+            for tr in ["\\ensuremath", "\\xspace", "\\aligned", "\\endaligned", "\\span"]:
                 string = string.replace(tr, "")
             
             # Correcting keywords written in an incorrect way
@@ -98,14 +74,58 @@ def Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, to_re
                     if sub[sub.find("\\mathbb")+7] != "{":
                         mathbb_parameter = sub[sub.find("\\newcommand")+12 : sub.find("}")].replace("\\", "")
                         sub = sub[:sub.find("\\mathbb")+7] + "{" + mathbb_parameter + "}" + sub[sub.find("\\mathbb")+7+len(mathbb_parameter):]
+                if "mathbf" in sub:
+                    if sub[sub.find("\\mathbf")+7] != "{":
+                        mathbf_parameter = sub[sub.find("\\newcommand")+12 : sub.find("}")].replace("\\", "")
+                        sub = sub[:sub.find("\\mathbf")+7] + "{" + mathbf_parameter + "}" + sub[sub.find("\\mathbf")+7+len(mathbf_parameter):]
                 
                 final_eqn += sub + " "     
-                                      
-            Latex_strs_json.append(final_eqn)
+            #print("final_eqn --> %s"%final_eqn)
+            
+            tempPath = "/projects/temporary/automates/er/gaurav/results_file/tempFile.txt"
+            json.dump(final_eqn, open(tempPath, "w"))
+            MML = Large_MML if EqnsType == "Large_eqns" else Small_MML
+            main(folder, file_name, tempPath, MML)
         except:
             pass
     
-    return(Latex_strs_json)
+def main(folder,file_name, tempPath, mml_path):
+    #print("main")
+    #print(eqn)
+    
+    # Define the webservice address
+    webservice = "http://localhost:8081"
+    # Load the LaTeX string data
+    eqn = json.load(open(tempPath, "r"))
+    #print(eqn)
+    #print("++==++"*15)
+    # Translate and save each LaTeX string using the NodeJS service for MathJax
+    #mml_strs = list()
+    
+    res = requests.post(
+        f"{webservice}/tex2mml",
+        headers={"Content-type": "application/json"},
+        json={"tex_src": json.dumps(eqn)},
+         )
+    #print(res.text)
+    # Capturing the keywords not supported by MathJax
+    if "FAILED" in res.content.decode("utf-8"):
+        # Just to check errors
+        TeXParseError = res.content.decode("utf-8").split("::")[1]
+        # Logging incorrect/ unsupported keywords along with their equations
+        if "Undefined control sequence" in TeXParseError:
+            Unsupported_Keyword = TeXParseError.split("\\")[-1]
+            if Unsupported_Keyword not in keywords_log:
+                keywords_log.append(Unsupported_Keyword)
+        # Logging errors other than unsupported keywords
+        else:
+            if TeXParseError not in Errors:
+                Errors.append(TeXParseError)
+                
+    else:
+        # Dump the MathML strings to JSON file
+        MML = CleaningMML(res.text)
+        json.dump(MML, open(os.path.join(mml_path, f"{file_name}.txt"), "w"))
 
 if __name__ == "__main__":
     # Paths
@@ -118,13 +138,15 @@ if __name__ == "__main__":
     for folder in os.listdir(folder_correct_latex_eqns):
         # Creating folder for MathML codes for specific file
         mml_folder = os.path.join(mml_dir, folder)
-        if not os.path.exists(mml_folder):
-            subprocess.call(['mkdir', mml_folder])
+        # Creating folder for Large and Small eqns
+        Large_MML = os.path.join(mml_folder, "Large_MML")
+        Small_MML = os.path.join(mml_folder, "Small_MML")
+        for F in [mml_folder, Large_MML, Small_MML]:
+            if not os.path.exists(F):
+                subprocess.call(['mkdir', F])
         
         # Creating Macros dictionary
         keyword_Macro_dict, keyword_dict = Creating_Macro_DMO_dictionaries(folder)
-        # Keywords to remove
-        to_remove = ["\\ensuremath", "\\xspace", "\\aligned", "\\endaligned", "\\span"]
         
         '''Appending all the eqns of the folder/paper to Latex_strs_json 
         along with their respective Macros and Declare Math Operator commands.'''
@@ -132,26 +154,14 @@ if __name__ == "__main__":
         Large_correct_eqns = os.path.join(folder_correct_latex_eqns, f"{folder}/Large_eqns")
         Small_correct_eqns = os.path.join(folder_correct_latex_eqns, f"{folder}/Small_eqns")
         for ce in [Large_correct_eqns, Small_correct_eqns]:
-            Latex_strs_json = Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, to_remove)
+            Creating_final_equations(ce, folder, keyword_Macro_dict, keyword_dict, Large_MML, Small_MML)
             
-            # Making directories and dumping Latex_strs_json
-            JSON_dir_folder = os.path.join(root, f"JSON_dir/{folder}")                           
-            if not os.path.exists(JSON_dir_folder):
-                subprocess.call(["mkdir", JSON_dir_folder])
-
-            # calling main(arg1, arg2) --> arg1: path to json file containing latex eqns
-            # args2: path to the json file to store converted mml eqns.      
-            if ce == Large_correct_eqns:
-                json.dump(Latex_strs_json, open(os.path.join(JSON_dir_folder, "LargeStrings.txt"),"w"))
-                main(folder, os.path.join(JSON_dir_folder, "LargeStrings.txt"), os.path.join(mml_folder, "LargeEqns_MML.txt"))
-            else:
-                json.dump(Latex_strs_json, open(os.path.join(JSON_dir_folder, "SmallStrings.txt"),"w"))
-                main(folder, os.path.join(JSON_dir_folder, "SmallStrings.txt"), os.path.join(mml_folder, "SmallEqns_MML.txt")) 
-     
+          
     print(keywords_log)
     print(" ")
     print(" ====================== Errors ======================")
     print(" ")
     print(Errors)
     json.dump(keywords_log, open("/projects/temporary/automates/er/gaurav/results_file/MathJax_Logs/Keywords_logs.txt", "w"))  
-    json.dump(keywords_log, open("/projects/temporary/automates/er/gaurav/results_file/MathJax_Logs/Errors_logs.txt", "w"))  
+    json.dump(Errors, open("/projects/temporary/automates/er/gaurav/results_file/MathJax_Logs/Errors_logs.txt", "w"))  
+
