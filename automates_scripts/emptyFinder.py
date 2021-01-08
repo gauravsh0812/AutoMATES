06c3201804d966e891b9bdbb9bc320c0e1a97109
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-01-07 21:00:54 -0700

    Update emptyFinder.py

--- automates_scripts/emptyFinder.py
+++ automates_scripts/emptyFinder.py
@@ -1,28 +1,33 @@
 import os, json, shutil 
 
-Files_to_check = []
+def main():
 
-for DIR in [1405]:
+        Files_to_check = []
 
-    path = f'/projects/temporary/automates/er/gaurav/2014/{DIR}/Mathjax_mml'
-    for Folder in os.listdir(path):
-        folder_path = os.path.join(path, Folder)
-        
-        
-        Large_path = os.path.join(folder_path, 'Large_MML')
-        Small_path = os.path.join(folder_path, 'Small_MML')
-        if os.path.exists(Large_path):
-            Large_files = len(os.listdir(Large_path))
-        else:
-            Files_to_check.append(Folder) 
-        if os.path.exists(Small_path):
-            Small_files = len(os.listdir(Small_path))
-        else:
-            Files_to_check.append(Folder) 
-            
-        Total = Large_files+Small_files
-        
-        if Total == 0:
-            Files_to_check.append(Folder)    
+        for DIR in [1405]:
 
-print(Files_to_check)
+            path = f'/projects/temporary/automates/er/gaurav/2014/{DIR}/Mathjax_mml'
+            for Folder in os.listdir(path):
+                folder_path = os.path.join(path, Folder)
+
+
+                Large_path = os.path.join(folder_path, 'Large_MML')
+                Small_path = os.path.join(folder_path, 'Small_MML')
+                if os.path.exists(Large_path):
+                    Large_files = len(os.listdir(Large_path))
+                else:
+                    Files_to_check.append(Folder) 
+                if os.path.exists(Small_path):
+                    Small_files = len(os.listdir(Small_path))
+                else:
+                    Files_to_check.append(Folder) 
+
+                Total = Large_files+Small_files
+
+                if Total == 0:
+                    Files_to_check.append(Folder)    
+
+        print(Files_to_check)
+
+if __name__=="__main__":
+    main()
